a695f203f3e2033e89a80dbbb64d840168e16dbf
chetvishal <vishal1982c@gmail.com>
2021-05-06 20:47:56 +0530

    refactored code

--- db/db.js
+++ db/db.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-    process.env.MONGO_URI,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-)
-    .then(() => console.log('Successfully connected to mongoDB'))
-    .catch(err => console.log("Error connecting to mongoDB: ", err))
+async function dbConnect() {
+    mongoose.connect(
+        process.env.MONGO_URI,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    )
+        .then(() => console.log('Successfully connected to mongoDB'))
+        .catch(err => console.log("Error connecting to mongoDB: ", err))
+}
 
 module.exports = { dbConnect }
